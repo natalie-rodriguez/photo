17330196f3d7ad505a33d1ee3e4678412b638b31
natalie <nataliediazrodriguez@gmail.com>
2019-07-22 20:45:07 -0400

    added create and show action

--- app/controllers/api/photos_controller.rb
+++ app/controllers/api/photos_controller.rb
@@ -1,2 +1,26 @@
 class Api::PhotosController < ApplicationController
-end
+  def index
+    # debugger
+    # show all the the pictures 
+    #get all the pictures from the database and show to the user 
+    @photos = Photo.all 
+    render "index.json.jb"
+  end
+
+  def show 
+    #show one particular picture 
+    #I need to get one picture fromm the database
+    # the_id = params[:id]
+    # @photo = Photo.find_by(id: the_id)
+    render "show.json.jb"
+  end 
+  def create 
+    @photo = Photo.new( 
+      name: params[:name],
+      width: params[:width],
+      height: params[:height])
+    # photo = Photo.new(name: "devante" , width: 8 , height:9 )
+    @photo.save
+    render "create.json.jb"
+  end 
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,19 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+   get "/photos" => "photos#index"
+   get "/photos/:id" => "photos#show"  
+   post "/photos"  => "photos#create"
+
+
+    # get "/photos" => "photos#index"
+    # get "/photos/new" => "photos#new"
+    # post "/photos"  => "photos#create" #c 
+   
+
+    # get "/photo/:id/edit" => "photo#edit"
+    # patch "/photos/:id" => "photo#update" #u 
+    # delete "/photos/:id" => "photos#destory" #D
+  end
 end
